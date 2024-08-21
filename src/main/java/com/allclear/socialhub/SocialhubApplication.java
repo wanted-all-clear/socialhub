c1bf1efbb963ca3c331e93a107dbf8f206e30913
youhyeoneee <khukyh2018@khu.ac.kr>
2024-08-21 14:26:14 +0900

    chore: spring boot 프로젝트 세팅

---
+++ src/main/java/com/allclear/socialhub/SocialhubApplication.java
@@ -0,0 +1,13 @@
+package com.allclear.socialhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allclear/socialhub/SocialhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allclear.socialhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
