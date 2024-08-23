5a0998b5f6fb2345092df360a66866b635ba1bb6
youhyeoneee <khukyh2018@khu.ac.kr>
2024-08-23 18:47:30 +0900

    [Style] 통계 에러 코드 네이밍 및 문구 변경
    - NOT_FOUND -> INVALID로 변경
    - 예외 문구 자세하게 변경
    - VALUE_TYPE -> TYPE으로 변경
    - 불필요한 공백 제거

--- src/main/java/com/allclear/socialhub/common/exception/ErrorCode.java
+++ src/main/java/com/allclear/socialhub/common/exception/ErrorCode.java
@@ -1,17 +1,16 @@
 package com.allclear.socialhub.common.exception;
 
-import org.springframework.http.HttpStatus;
-
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
 
-	// USER
-	USER_NOT_EXIST(HttpStatus.NOT_FOUND, "가입되지 않은 아이디입니다."),
-	USERNAME_DUPLICATION(HttpStatus.BAD_REQUEST, "중복된 아이디입니다."),
-	PASSWORD_NOT_VALID(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
-	INVALID_PASSWORD_PATTERN(HttpStatus.BAD_REQUEST, "비밀번호는 10자 이상 16자 이하의 길이의 영문자, 숫자, 특수문자(!@#$%^&*)만 사용 가능합니다."),
+    // USER
+    USER_NOT_EXIST(HttpStatus.NOT_FOUND, "가입되지 않은 아이디입니다."),
+    USERNAME_DUPLICATION(HttpStatus.BAD_REQUEST, "중복된 아이디입니다."),
+    PASSWORD_NOT_VALID(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    INVALID_PASSWORD_PATTERN(HttpStatus.BAD_REQUEST, "비밀번호는 10자 이상 16자 이하의 길이의 영문자, 숫자, 특수문자(!@#$%^&*)만 사용 가능합니다."),
 
     // POST
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게시물입니다."),
@@ -20,19 +19,19 @@ public enum ErrorCode {
 
 
     // STATISTICS
-    STATISTICS_TYPE_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 통계 타입입니다. expected: [date, hour]"),
-    STATISTICS_VALUE_TYPE_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 통계 데이터 타입입니다. expected: [count, view_count, like_count, share_count]"),
-    STATISTICS_INVALID_DATE_PATTERN(HttpStatus.BAD_REQUEST, "날짜 형식이 올바르지 않습니다. expected: YY-MM-DD"),
+    STATISTICS_INVALID_TYPE(HttpStatus.BAD_REQUEST, "유효하지 않은 type parameter 입니다. expected: ['date', 'hour']"),
+    STATISTICS_INVALID_VALUE(HttpStatus.BAD_REQUEST, "유효하지 않은 value parameter 입니다. expected: ['count', 'view_count', 'like_count', 'share_count']"),
+    STATISTICS_INVALID_DATE_PATTERN(HttpStatus.BAD_REQUEST, "유효하지 않은 날짜 형식입니다. expected: 'YYYY-MM-DD'"),
     STATISTICS_INVALID_DATE_VALUE(HttpStatus.BAD_REQUEST, "잘못된 날짜입니다."),
-    STATISTICS_INVALID_DATE_DURATION_DATE (HttpStatus.BAD_REQUEST, "최대 30일까지만 조회할 수 있습니다."),
-    STATISTICS_INVALID_DATE_DURATION_HOUR (HttpStatus.BAD_REQUEST, "최대 7일까지만 조회할 수 있습니다.");
+    STATISTICS_INVALID_DATE_DURATION_DATE(HttpStatus.BAD_REQUEST, "최대 30일까지만 조회할 수 있습니다."),
+    STATISTICS_INVALID_DATE_DURATION_HOUR(HttpStatus.BAD_REQUEST, "최대 7일까지만 조회할 수 있습니다.");
 
-	private final HttpStatus httpStatus;
-	private final String message;
+    private final HttpStatus httpStatus;
+    private final String message;
 
-	ErrorCode(HttpStatus httpStatus, String message) {
+    ErrorCode(HttpStatus httpStatus, String message) {
 
-		this.httpStatus = httpStatus;
-		this.message = message;
-	}
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
 }
