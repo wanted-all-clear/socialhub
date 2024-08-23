6915d750ae8b9e1f5045455160b5ffc1eab449e9
youhyeoneee <khukyh2018@khu.ac.kr>
2024-08-23 18:53:26 +0900

    [Feat] getStatisticsDaily, getStatisticsHourly 메서드 파라미터 추가

--- src/main/java/com/allclear/socialhub/post/service/StatisticService.java
+++ src/main/java/com/allclear/socialhub/post/service/StatisticService.java
@@ -1,9 +1,16 @@
 package com.allclear.socialhub.post.service;
 
+import com.allclear.socialhub.post.domain.StatisticType;
+import com.allclear.socialhub.post.domain.StatisticValue;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public interface StatisticService {
-    void getStatisticsDaily();
-    void getStatisticsHourly();
+
+    void getStatisticsDaily(String hashtag, StatisticType type, LocalDate start, LocalDate end, StatisticValue value);
+
+    void getStatisticsHourly(String hashtag, StatisticType type, LocalDate start, LocalDate end, StatisticValue value);
+
 }
--- src/main/java/com/allclear/socialhub/post/service/StatisticServiceImpl.java
+++ src/main/java/com/allclear/socialhub/post/service/StatisticServiceImpl.java
@@ -1,14 +1,23 @@
 package com.allclear.socialhub.post.service;
 
+import com.allclear.socialhub.post.domain.StatisticType;
+import com.allclear.socialhub.post.domain.StatisticValue;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
 public class StatisticServiceImpl implements StatisticService {
 
-    @Override
-    public void getStatisticsDaily() {
 
+    @Override
+    public void getStatisticsDaily(String hashtag, StatisticType type, LocalDate start, LocalDate end, StatisticValue value) {
+        
     }
 
     @Override
-    public void getStatisticsHourly() {
+    public void getStatisticsHourly(String hashtag, StatisticType type, LocalDate start, LocalDate end, StatisticValue value) {
 
     }
+
 }
