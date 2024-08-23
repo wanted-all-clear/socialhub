c801a1ee3546b51ac83f50917423710470992c8c
Eunjung <fkznsha23@gmail.com>
2024-08-23 10:21:52 +0900

    [Refactor] User 엔티티 줄바꿈 수정

--- src/main/java/com/allclear/socialhub/user/domain/User.java
+++ src/main/java/com/allclear/socialhub/user/domain/User.java
@@ -1,15 +1,23 @@
 package com.allclear.socialhub.user.domain;
 
+import java.time.LocalDateTime;
+
 import com.allclear.socialhub.common.domain.Timestamped;
 import com.allclear.socialhub.user.type.UserCertifyStatus;
 import com.allclear.socialhub.user.type.UserStatus;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -18,20 +26,27 @@ import java.time.LocalDateTime;
 @Builder
 @Table(name = "user")
 public class User extends Timestamped {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(nullable = false)
-    private String userName;
-    @Column(nullable = false)
-    private String email;
-    @Column(nullable = false)
-    private String password;
-    private LocalDateTime deletedAt;
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private UserStatus userStatus;
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private UserCertifyStatus certifyStatus;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String userName;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	private LocalDateTime deletedAt;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private UserStatus userStatus;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private UserCertifyStatus certifyStatus;
 }
