b130ae7b9c91bae46bb1abf15a43501a5c8060c9
hyojin <dongurami.kim@gmail.com>
2024-08-23 20:04:27 +0900

    [Feat] statistics daily 에 필요한 query 추가

--- src/main/java/com/allclear/socialhub/post/common/hashtag/repository/HashTagRepository.java
+++ src/main/java/com/allclear/socialhub/post/common/hashtag/repository/HashTagRepository.java
@@ -2,6 +2,17 @@ package com.allclear.socialhub.post.common.hashtag.repository;
 
 import com.allclear.socialhub.post.common.hashtag.domain.HashTag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface HashTagRepository extends JpaRepository<HashTag, Long> {
+
+    @Query("SELECT ph.post.id " +
+            "FROM HashTag AS h " +
+            "INNER JOIN PostHashTag AS ph on h.id = ph.hashTag.id " +
+            "where h.content = :hashtag")
+    List<Long> getPostByHashtag(@Param("hashtag") String hashtag);
+
 }
