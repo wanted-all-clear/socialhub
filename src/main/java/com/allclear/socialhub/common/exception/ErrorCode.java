12a0aae42117421e265bc9d63b7a033b09cc7d46
hyojin <dongurami.kim@gmail.com>
2024-08-25 00:30:45 +0900

    [Refactor] ErrorCode 원복

--- src/main/java/com/allclear/socialhub/common/exception/ErrorCode.java
+++ src/main/java/com/allclear/socialhub/common/exception/ErrorCode.java
@@ -6,23 +6,35 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-	// USER
-	USER_NOT_EXIST(HttpStatus.NOT_FOUND, "가입되지 않은 아이디입니다."),
-	USERNAME_DUPLICATION(HttpStatus.BAD_REQUEST, "중복된 아이디입니다."),
-	PASSWORD_NOT_VALID(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
-	INVALID_PASSWORD_PATTERN(HttpStatus.BAD_REQUEST, "비밀번호는 10자 이상 16자 이하의 길이의 영문자, 숫자, 특수문자(!@#$%^&*)만 사용 가능합니다."),
+    // USER
+    USER_NOT_EXIST(HttpStatus.NOT_FOUND, "가입되지 않은 아이디입니다."),
+    EMAIL_DUPLICATION(HttpStatus.CONFLICT, "중복된 이메일입니다."),
+    USERNAME_DUPLICATION(HttpStatus.BAD_REQUEST, "중복된 계정명입니다."),
+    PASSWORD_NOT_VALID(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    INVALID_USERNAME_LENGTH(HttpStatus.BAD_REQUEST, "계정명은 3자 이상 20자 이하로 설정해야 합니다."),
+    INVALID_PASSWORD_LENGTH(HttpStatus.BAD_REQUEST, "비밀번호는 10자 이상 설정해야 합니다."),
+    INVALID_PASSWORD_PATTERN(HttpStatus.BAD_REQUEST, "비밀번호는 10자 이상, 20자 이하 영문자, 숫자, 특수문자(!@#$%^&*)중 2가지 이상 포함해야 합니다."),
 
-	// POST
-	POST_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게시물입니다."),
-	POST_NOT_OWNER(HttpStatus.BAD_REQUEST, "본인 글만 수정/삭제가 가능합니다."),
-	POST_TYPE_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 게시물 타입입니다.");
+    // POST
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게시물입니다."),
+    POST_NOT_OWNER(HttpStatus.BAD_REQUEST, "본인 글만 수정/삭제가 가능합니다."),
+    POST_TYPE_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 게시물 타입입니다."),
+
+    // STATISTICS
+    STATISTICS_INVALID_TYPE(HttpStatus.BAD_REQUEST, "유효하지 않은 type parameter 입니다. expected: ['date', 'hour']"),
+    STATISTICS_INVALID_VALUE(HttpStatus.BAD_REQUEST, "유효하지 않은 value parameter 입니다. expected: ['count', 'view_count', 'like_count', 'share_count']"),
+    STATISTICS_INVALID_DATE_PATTERN(HttpStatus.BAD_REQUEST, "유효하지 않은 날짜 형식입니다. expected: 'YYYY-MM-DD'"),
+    STATISTICS_INVALID_DATE_VALUE(HttpStatus.BAD_REQUEST, "잘못된 날짜입니다."),
+    STATISTICS_INVALID_DATE_DURATION_DATE(HttpStatus.BAD_REQUEST, "최대 30일까지만 조회할 수 있습니다."),
+    STATISTICS_INVALID_DATE_DURATION_HOUR(HttpStatus.BAD_REQUEST, "최대 7일까지만 조회할 수 있습니다.");
+
 
     private final HttpStatus httpStatus;
     private final String message;
 
-	ErrorCode(HttpStatus httpStatus, String message) {
+    ErrorCode(HttpStatus httpStatus, String message) {
 
-		this.httpStatus = httpStatus;
-		this.message = message;
-	}
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
 }
