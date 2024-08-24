03187f2ccb4f64c86327aad9ac1bcb3c46f88bce
youhyeoneee <khukyh2018@khu.ac.kr>
2024-08-24 11:40:26 +0900

    [Feat] 통계 일자별 API 컨트롤러 구현

--- src/main/java/com/allclear/socialhub/post/controller/StatisticController.java
+++ src/main/java/com/allclear/socialhub/post/controller/StatisticController.java
@@ -2,6 +2,7 @@ package com.allclear.socialhub.post.controller;
 
 import com.allclear.socialhub.post.domain.StatisticType;
 import com.allclear.socialhub.post.domain.StatisticValue;
+import com.allclear.socialhub.post.dto.StatisticResponse;
 import com.allclear.socialhub.post.service.StatisticService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts/statistics")
@@ -22,7 +24,7 @@ public class StatisticController {
     private final StatisticService statisticService;
 
     @GetMapping
-    public void getStatistics(
+    public List<StatisticResponse> getStatistics(
             @RequestParam(name = "hashtag", required = false) String hashtag,
             @RequestParam(name = "type", defaultValue = "date") StatisticType type,
             @RequestParam(name = "start", defaultValue = "#{T(java.time.LocalDate).now().minusDays(7)}") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
@@ -30,19 +32,18 @@ public class StatisticController {
             @RequestParam(name = "value", defaultValue = "count") StatisticValue value
     ) {
 
-        // 예외처리
-        // 유효성검사
         // TODO: hashtag = null일 경우 본인계정으로 설정
-        log.info("hashtag : " + "hashtag");
-
+        log.info("hashtag : " + hashtag);
         log.info("type : " + type);
         log.info(start + "~" + end);
         log.info("value : " + value);
         if (type == StatisticType.DATE) {
-            statisticService.getStatisticsDaily(hashtag, type, start, end, value);
+            return statisticService.getStatisticsDaily(hashtag, type, start, end, value);
+
         } else if (type == StatisticType.HOUR) {
-            statisticService.getStatisticsHourly(hashtag, type, start, end, value);
+            return statisticService.getStatisticsHourly(hashtag, type, start, end, value);
         }
+        return null;
     }
 
 }
