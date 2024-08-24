438083a24403aa1c2945b503430bc254c969c9e1
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-24 18:21:31 +0900

    [Style] 들여쓰기 수정

--- src/main/java/com/allclear/socialhub/post/repository/querydsl/PostRepositoryImpl.java
+++ src/main/java/com/allclear/socialhub/post/repository/querydsl/PostRepositoryImpl.java
@@ -1,78 +1,76 @@
 package com.allclear.socialhub.post.repository.querydsl;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-
 import com.allclear.socialhub.post.common.hashtag.domain.QHashtag;
 import com.allclear.socialhub.post.common.hashtag.domain.QPostHashtag;
 import com.allclear.socialhub.post.domain.QPost;
 import com.allclear.socialhub.post.dto.PostListResponse;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-
 import jakarta.persistence.EntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PostRepositoryImpl implements PostRepositoryQuerydsl {
 
-	private final JPAQueryFactory queryFactory;
+    private final JPAQueryFactory queryFactory;
 
-	public PostRepositoryImpl(EntityManager em) {
+    public PostRepositoryImpl(EntityManager em) {
 
-		this.queryFactory = new JPAQueryFactory(em);
-	}
+        this.queryFactory = new JPAQueryFactory(em);
+    }
 
-	public Page<PostListResponse> getPosts(Pageable pageable) {
+    public Page<PostListResponse> getPosts(Pageable pageable) {
 
-		QPost post = QPost.post;
-		QHashtag hashtag = QHashtag.hashtag;
-		QPostHashtag postHashtag = QPostHashtag.postHashtag;
+        QPost post = QPost.post;
+        QHashtag hashtag = QHashtag.hashtag;
+        QPostHashtag postHashtag = QPostHashtag.postHashtag;
 
-		List<PostListResponse> postList = queryFactory
-				.select(
-						Projections.bean(
-								PostListResponse.class,
-								post.id.as("postId"),
-								post.type,
-								post.title,
-								post.content,
-								post.viewCnt,
-								post.likeCnt,
-								post.shareCnt,
-								post.createdAt,
-								post.updatedAt
-						)
-				)
-				.from(post)
-				.leftJoin(postHashtag).on(postHashtag.post.id.eq(post.id))
-				.leftJoin(hashtag).on(hashtag.id.eq(postHashtag.hashTag.id))
-				.groupBy(post.id)
-				.orderBy(post.id.desc())
-				.offset(pageable.getOffset())
-				.limit(pageable.getPageSize())
-				.fetch()
-				.stream()
-				.map(postListResponse -> {
-					List<String> hashtagList = queryFactory
-							.select(hashtag.content)
-							.from(postHashtag)
-							.join(hashtag).on(hashtag.id.eq(postHashtag.hashTag.id))
-							.where(postHashtag.post.id.eq(postListResponse.getPostId()))
-							.fetch();
-					postListResponse.setHashtagList(hashtagList);
+        List<PostListResponse> postList = queryFactory
+                .select(
+                        Projections.bean(
+                                PostListResponse.class,
+                                post.id.as("postId"),
+                                post.type,
+                                post.title,
+                                post.content,
+                                post.viewCnt,
+                                post.likeCnt,
+                                post.shareCnt,
+                                post.createdAt,
+                                post.updatedAt
+                        )
+                )
+                .from(post)
+                .leftJoin(postHashtag).on(postHashtag.post.id.eq(post.id))
+                .leftJoin(hashtag).on(hashtag.id.eq(postHashtag.hashTag.id))
+                .groupBy(post.id)
+                .orderBy(post.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch()
+                .stream()
+                .map(postListResponse -> {
+                    List<String> hashtagList = queryFactory
+                            .select(hashtag.content)
+                            .from(postHashtag)
+                            .join(hashtag).on(hashtag.id.eq(postHashtag.hashTag.id))
+                            .where(postHashtag.post.id.eq(postListResponse.getPostId()))
+                            .fetch();
+                    postListResponse.setHashtagList(hashtagList);
 
-					return postListResponse;
-				})
-				.collect(Collectors.toList());
+                    return postListResponse;
+                })
+                .collect(Collectors.toList());
 
-		long total = queryFactory
-				.selectFrom(post)
-				.fetchCount();
+        long total = queryFactory
+                .selectFrom(post)
+                .fetchCount();
 
-		return new PageImpl<>(postList, pageable, total);
-	}
+        return new PageImpl<>(postList, pageable, total);
+    }
 
 }
--- src/main/java/com/allclear/socialhub/post/service/PostServiceImpl.java
+++ src/main/java/com/allclear/socialhub/post/service/PostServiceImpl.java
@@ -1,12 +1,5 @@
 package com.allclear.socialhub.post.service;
 
-import static com.allclear.socialhub.common.exception.ErrorCode.*;
-
-import java.util.List;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-
 import com.allclear.socialhub.common.exception.CustomException;
 import com.allclear.socialhub.post.common.hashtag.domain.Hashtag;
 import com.allclear.socialhub.post.common.hashtag.domain.PostHashtag;
@@ -19,57 +12,62 @@ import com.allclear.socialhub.post.dto.PostResponse;
 import com.allclear.socialhub.post.repository.PostRepository;
 import com.allclear.socialhub.user.domain.User;
 import com.allclear.socialhub.user.repository.UserRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.allclear.socialhub.common.exception.ErrorCode.USER_NOT_EXIST;
 
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
 
-	private final UserRepository userRepository;
-	private final HashtagService hashTagService;
-	private final PostRepository postRepository;
-	private final PostHashtagRepository postHashtagRepository;
+    private final UserRepository userRepository;
+    private final HashtagService hashTagService;
+    private final PostRepository postRepository;
+    private final PostHashtagRepository postHashtagRepository;
 
-	/**
-	 * 1. 게시물 등록
-	 * 작성자 : 오예령
-	 *
-	 * @param requestPostDto
-	 * @return
-	 */
-	@Override
-	public PostResponse createPost(Long userId, PostCreateRequest requestPostDto) {
-		// 1. 유저 검증
-		User user = userRepository.findById(userId).orElseThrow(
-				() -> new CustomException(USER_NOT_EXIST)
-		);
-		// 2. 게시물 등록
-		Post post = postRepository.save(requestPostDto.toEntity(user));
+    /**
+     * 1. 게시물 등록
+     * 작성자 : 오예령
+     *
+     * @param requestPostDto
+     * @return
+     */
+    @Override
+    public PostResponse createPost(Long userId, PostCreateRequest requestPostDto) {
+        // 1. 유저 검증
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new CustomException(USER_NOT_EXIST)
+        );
+        // 2. 게시물 등록
+        Post post = postRepository.save(requestPostDto.toEntity(user));
 
-		// 3. 해시태그 등록
-		List<Hashtag> savedHashtags = hashTagService.toEachHashTag(requestPostDto.getHashtagList());
+        // 3. 해시태그 등록
+        List<Hashtag> savedHashtags = hashTagService.toEachHashTag(requestPostDto.getHashtagList());
 
-		for (Hashtag hashTag : savedHashtags) {
-			PostHashtag postHashTag = PostHashtag.builder()
-					.post(post)
-					.hashTag(hashTag)
-					.build();
-			postHashtagRepository.save(postHashTag);
-		}
-		return PostResponse.fromEntity(post, requestPostDto.getHashtagList());
-	}
+        for (Hashtag hashTag : savedHashtags) {
+            PostHashtag postHashTag = PostHashtag.builder()
+                    .post(post)
+                    .hashTag(hashTag)
+                    .build();
+            postHashtagRepository.save(postHashTag);
+        }
+        return PostResponse.fromEntity(post, requestPostDto.getHashtagList());
+    }
 
-	/**
-	 * 5. 게시물 목록 조회
-	 * 작성자 : 유리빛나
-	 *
-	 * @param pageable Pagination 요청 정보 관련 인터페이스
-	 * @return 페이징 처리가 된 게시물 전체 목록
-	 */
-	public PostPaging getPosts(Pageable pageable) {
+    /**
+     * 5. 게시물 목록 조회
+     * 작성자 : 유리빛나
+     *
+     * @param pageable Pagination 요청 정보 관련 인터페이스
+     * @return 페이징 처리가 된 게시물 전체 목록
+     */
+    public PostPaging getPosts(Pageable pageable) {
 
-		return new PostPaging(postRepository.getPosts(pageable));
-	}
+        return new PostPaging(postRepository.getPosts(pageable));
+    }
 
 }
