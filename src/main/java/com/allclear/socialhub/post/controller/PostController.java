0940e7746312b24d51c4b66fe02f65dbdb15839a
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-24 18:25:39 +0900

    [Style] 들여쓰기 수정

--- src/main/java/com/allclear/socialhub/post/controller/PostController.java
+++ src/main/java/com/allclear/socialhub/post/controller/PostController.java
@@ -1,40 +1,34 @@
 package com.allclear.socialhub.post.controller;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.allclear.socialhub.post.dto.PostCreateRequest;
 import com.allclear.socialhub.post.dto.PostPaging;
 import com.allclear.socialhub.post.dto.PostResponse;
 import com.allclear.socialhub.post.service.PostService;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/posts")
 public class PostController {
 
-	private final PostService postService;
+    private final PostService postService;
 
-	@RequestMapping(value = "", method = RequestMethod.POST)
-	public ResponseEntity<PostResponse> creatPost(@RequestBody PostCreateRequest requestDto) {
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<PostResponse> creatPost(@RequestBody PostCreateRequest requestDto) {
 
-		// TODO : 유저 받아오는 형식 추후 변경 예정
-		return ResponseEntity.status(201).body(postService.createPost(1L, requestDto));
-	}
+        // TODO : 유저 받아오는 형식 추후 변경 예정
+        return ResponseEntity.status(201).body(postService.createPost(1L, requestDto));
+    }
 
-	@GetMapping
-	public PostPaging getPosts(@PageableDefault Pageable pageable) {
+    @GetMapping
+    public PostPaging getPosts(@PageableDefault Pageable pageable) {
 
-		return postService.getPosts(pageable);
+        return postService.getPosts(pageable);
 
-	}
+    }
 
 }
