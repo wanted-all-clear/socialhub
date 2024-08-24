ec384a6dedd0721b3d0ae6803123e24fbd3775d9
bsjin1122 <kitty7579@hanmail.net>
2024-08-24 09:17:32 +0900

    [Chore] EnvConfig 주석처리 및 issue 등록 폼 문구 수정

--- src/main/java/com/allclear/socialhub/env/EnvConfig.java
+++ src/main/java/com/allclear/socialhub/env/EnvConfig.java
@@ -1,9 +1,9 @@
 package com.allclear.socialhub.env;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:.env")
+//@PropertySource("classpath:.env")
 public class EnvConfig {
+
 }
