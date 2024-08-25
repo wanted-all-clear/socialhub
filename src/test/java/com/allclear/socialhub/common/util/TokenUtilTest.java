d256fa5975a0e4b76bc3e679ae351ee46b6081f5
youhyeoneee <khukyh2018@khu.ac.kr>
2024-08-26 04:15:47 +0900

    [Test] TokenUtil Bearer 제거 메서드 단위 테스트 추가

---
+++ src/test/java/com/allclear/socialhub/common/util/TokenUtilTest.java
@@ -0,0 +1,23 @@
+package com.allclear.socialhub.common.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TokenUtilTest {
+
+    @Test
+    @DisplayName("토큰 제거")
+    void removeBearer() {
+        // given
+        String header = "Bearer sometokenssssss";
+
+        // when
+        String result = TokenUtil.removeBearer(header);
+
+        // then
+        assertEquals(result, "sometokenssssss");
+    }
+
+}
