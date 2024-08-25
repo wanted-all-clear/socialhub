9f790471ecf9ef78d3a789e67ffa6f1c4a7be26d
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-25 20:54:20 +0900

    [Refactor] 게시물 등록 예외 처리 추가

--- src/main/java/com/allclear/socialhub/post/dto/PostCreateRequest.java
+++ src/main/java/com/allclear/socialhub/post/dto/PostCreateRequest.java
@@ -3,6 +3,7 @@ package com.allclear.socialhub.post.dto;
 import com.allclear.socialhub.post.domain.Post;
 import com.allclear.socialhub.post.domain.PostType;
 import com.allclear.socialhub.user.domain.User;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,7 +18,9 @@ import java.util.List;
 public class PostCreateRequest {
 
     private PostType type;
+    @NotNull(message = "제목은 필수 입력 값입니다.")
     private String title;
+    @NotNull(message = "내용은 필수 입력 값입니다.")
     private String content;
     private List<String> hashtagList;
 
