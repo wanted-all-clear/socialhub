e275b15985a985416ab3832cd84c37b94ef89a38
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-08-26 07:05:43 +0900

    [Style] PostDetailResponse DTO에 Builder 추가
    - Controller 테스트 시 필요하여 추가하였습니다.

--- src/main/java/com/allclear/socialhub/post/dto/PostDetailResponse.java
+++ src/main/java/com/allclear/socialhub/post/dto/PostDetailResponse.java
@@ -2,16 +2,14 @@ package com.allclear.socialhub.post.dto;
 
 import com.allclear.socialhub.post.domain.PostType;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostDetailResponse {
@@ -32,6 +30,7 @@ public class PostDetailResponse {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedAt;
 
+    // QueryDsl 에서 사용되는 Builder
     public PostDetailResponse(Long postId, Long userId, PostType type, String title, String content, int viewCnt, int likeCnt, int shareCnt, LocalDateTime createdAt, LocalDateTime updatedAt) {
 
         this.postId = postId;
