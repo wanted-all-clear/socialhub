3a8cfbc49437bfe121829210ab34ed3adec518e2
bsjin1122 <kitty7579@hanmail.net>
2024-08-25 21:42:11 +0900

    [Feat] User 컬럼 비밀번호 (length) 길이 제한 삭제, 회원가입 비밀번호 암호화 로직 추가

--- src/main/java/com/allclear/socialhub/user/domain/User.java
+++ src/main/java/com/allclear/socialhub/user/domain/User.java
@@ -1,7 +1,5 @@
 package com.allclear.socialhub.user.domain;
 
-import java.time.LocalDateTime;
-
 import com.allclear.socialhub.common.domain.Timestamped;
 import com.allclear.socialhub.user.type.UserCertifyStatus;
 import com.allclear.socialhub.user.type.UserStatus;
@@ -10,19 +8,13 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -32,29 +24,30 @@ import lombok.NoArgsConstructor;
 @Table(name = "user")
 public class User extends Timestamped {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(nullable = false, length = 20)
-	private String username;
+    @Column(nullable = false, length = 20)
+    private String username;
 
-	@Column(nullable = false)
-	private String email;
+    @Column(nullable = false)
+    private String email;
 
-	@Column(nullable = false, length = 20)
-	private String password;
+    @Column(nullable = false)
+    private String password;
 
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	@JsonSerialize(using = LocalDateTimeSerializer.class)
-	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
-	private LocalDateTime deletedAt;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime deletedAt;
 
-	@Column(nullable = false)
-	@Enumerated(EnumType.STRING)
-	private UserStatus status;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
 
-	@Column(nullable = false)
-	@Enumerated(EnumType.STRING)
-	private UserCertifyStatus certifyStatus;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserCertifyStatus certifyStatus;
+
 }
